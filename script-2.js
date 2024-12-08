4e6878cf3630eb6587b3b6118b97e7d929fbe938
Andreawingardh <andreawingardh@gmail.com>
2024-12-08 12:48:17 +0100

    Edited the script to make one function for all three meal categories

--- script-2.js
+++ script-2.js
@@ -1,14 +1,17 @@
   /* Script for the second page called categories, where only a meal is displayed along with a link to the webpage. */
 
 
-  const displayMeal = () => {
-    document.getElementById('display-meal-vegetarian').addEventListener('click', () => {
-        fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian')
+  const displayMeal = (category, ElementId) => {
+    document.getElementById(ElementId).addEventListener('click', () => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
         .then((response) => {
             return response.json();
         })
         .then((getVegetarianMeal) => {
+            /* Check if the fetch has worked */
             console.log(getVegetarianMeal);
+
+            /*Creates a random */
             const ingredientKey = Math.floor(Math.random() * getVegetarianMeal.meals.length + 1);
             const randomRecipe = getVegetarianMeal.meals[ingredientKey];
             console.log(randomRecipe);
@@ -20,72 +23,30 @@
             displayMeal.appendChild(mealImage);
             mealImage.src = randomRecipe.strMealThumb;
             mealTitle.textContent = randomRecipe.strMeal;
+            fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomRecipe.idMeal}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then ((mealDetails) => {
+                console.log(mealDetails.meals);
+
+            })
+
+            .catch(error => {
+                console.log(error);
+            });
 
         })
+
         .catch(error => {
             console.log(error);
         })
     })
 }
 
-const displayMealChicken = () => {
-    document.getElementById('display-meal-chicken').addEventListener('click', () => {
-        fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken')
-        .then((response) => {
-            return response.json();
-        })
-        .then((getVegetarianMeal) => {
-            console.log(getVegetarianMeal);
-            const ingredientKey = Math.floor(Math.random() * getVegetarianMeal.meals.length + 1);
-            const randomRecipe = getVegetarianMeal.meals[ingredientKey];
-            console.log(randomRecipe);
-            const displayMeal = document.getElementById('category-displayMeal');
-            displayMeal.innerHTML = '';
-            const mealTitle = document.createElement('h2');
-            displayMeal.appendChild(mealTitle);
-            mealTitle.textContent = randomRecipe.strMeal;
-            const mealImage = document.createElement('img');
-            displayMeal.appendChild(mealImage);
-            mealImage.src = randomRecipe.strMealThumb;
-
-
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    })
-}
-
-const displayMealDessert = () => {
-    document.getElementById('display-meal-dessert').addEventListener('click', () => {
-        fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert')
-        .then((response) => {
-            return response.json();
-        })
-        .then((getVegetarianMeal) => {
-            console.log(getVegetarianMeal);
-            const ingredientKey = Math.floor(Math.random() * getVegetarianMeal.meals.length + 1);
-            const randomRecipe = getVegetarianMeal.meals[ingredientKey];
-            console.log(randomRecipe);
-            const displayMeal = document.getElementById('category-displayMeal');
-            displayMeal.innerHTML = '';
-            const mealTitle = document.createElement('h2');
-            displayMeal.appendChild(mealTitle);
-            mealTitle.textContent = randomRecipe.strMeal;
-            const mealImage = document.createElement('img');
-            displayMeal.appendChild(mealImage);
-            mealImage.src = randomRecipe.strMealThumb;
-
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    })
-}
-
-displayMeal();
-displayMealChicken();
-displayMealDessert();
+displayMeal('Vegetarian', 'display-meal-vegetarian');
+displayMeal('Chicken', 'display-meal-chicken');
+displayMeal('Dessert', 'display-meal-dessert');
 
 /* For next level categories, looking up meal details */
 // fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + randomRecipe.idMeal)
